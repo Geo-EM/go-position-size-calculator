39dd3df25fd61a4f3fa2abebe151a8e6daed6c86
Geo-EM <georgeem17821@gmail.com>
2026-03-10 17:11:59 +0200

    Add PositionInput & PositionResult structs

--- internal/calculator/position.go
+++ internal/calculator/position.go
@@ -1,1 +1,12 @@
 package calculator
+
+type PositionInput struct {
+	Risk  float64
+	Entry float64
+	Stop  float64
+}
+
+type PositionResult struct {
+	Shares       float64
+	RiskPerShare float64
+}
